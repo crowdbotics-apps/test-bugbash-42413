9dbb79effd8a24844371264faeb7bc6087be262e
Deepak Narang <deepak.narang.dms@crowdbotics.com>
2023-07-25 20:13:35 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "test-bugbash-42413",
-  url: "https://test-bugbash-42413.botics.co",
-  api: "https://test-bugbash-42413.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "test-bugbash-42413",
+  "url": "https://test-bugbash-42413.botics.co",
+  "api": "https://test-bugbash-42413.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "aboutTheApp"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
---
+++ screens/Untitled5/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled5 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><View style={styles.ndvyIobK}></View></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  ndvyIobK: {
+    height: 122,
+    width: 332,
+    backgroundColor: "#ff0000",
+    borderRadius: 40,
+    color: "#777777"
+  }
+});
+export default Untitled5;
